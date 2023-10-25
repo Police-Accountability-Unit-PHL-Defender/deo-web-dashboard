9e57e27dc48ddf1cb14ccbdb41a2074fdc2f4816
Taichi Aritomo <taichiaritomo@gmail.com>
2023-10-24 23:57:39 -0400

    First commit, web app setup

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <Header></Header>
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Header from '~/components/Header.vue'
+</script>
---
+++ pages/map.vue
@@ -0,0 +1,7 @@
+<template>
+  <CityCouncilDistrictsMap />
+  </template>
+  
+  <script setup lang="ts">
+  import CityCouncilDistrictsMap from '~/components/map/CityCouncilDistrictsMap.vue';
+</script>
