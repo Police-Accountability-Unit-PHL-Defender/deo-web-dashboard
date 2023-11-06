e80841ce651c76ee70d5f7408c38c99f8fe26dcf
Taichi Aritomo <taichiaritomo@gmail.com>
2023-11-05 20:10:20 -0500

    First d3 plot

--- pages/stops.vue
+++ pages/stops.vue
@@ -1,4 +1,7 @@
 <template>
+  <p class="my-12 text-body-4">
+    This is a WIP to demo UI state management, CSV parsing, front-end data aggregation, and plotting. The data used is only for PSA 91. Different filters do not yet update the results.
+  </p>
   <div class="text-body-1">
     How many
     <SelectEvent v-model="selectedEvent"/>
@@ -19,10 +22,10 @@
     Philadelphia police
     <SelectedResult>{{ selectedEvent }}</SelectedResult>
     a total of
-    <SelectedResult>400 vehicles</SelectedResult>.
+    <SelectedResult>{{ question1Total }} {{ selectedEvent }}</SelectedResult>.
   </div>
 
-  {{ question1Data }}
+  <svg></svg>
 </template>
 
 <script setup>
@@ -30,12 +33,14 @@ import SelectEvent from '~/components/SelectEvent.vue'
 import SelectLocation from '~/components/SelectLocation.vue'
 import SelectTimeGranularity from '~/components/SelectTimeGranularity.vue'
 import SelectedResult from '~/components/ui/SelectedResult.vue';
+import * as d3 from 'd3'
 
 const selectedEvent = ref('stops')
 const selectedLocation = ref('citywide')
 const selectedTimeGranularity = ref('every year')
 const selectedTime0 = ref(2015)
 const selectedTime1 = ref(2023)
+const question1Total = ref(0)
 
 const data = ref([])
 
@@ -44,14 +49,12 @@ watch(selectedLocation, () => {
   data.value = getDataFromCSV('/data/open_data_philly_psa-091.csv')
 })
 
-const question1Data = computed(() => {
-  console.log('hi')
-  console.log(data)
-  const filteredData = data.value.filter(d => d.year >= selectedTime0 && d.year <= selectedTime1)
-  const groupedData = groupBy(filteredData, 'year')
-  const reducedData = objectMap(groupedData, group => group.length)
-  console.log(reducedData)
-})
+const parseTime = d3.timeParse("%y");
+
+// const question1Data = computed(() => {
+  
+//   // console.log(graphData)
+// })
 
 onMounted(async () => {
   // data.value = await getDataFromCSV('/data/open_data_philly_psa-091.csv')
@@ -60,11 +63,85 @@ onMounted(async () => {
     .then(response => response.text())
     .then(text => {
       const csv = CSVToArrayOfObjects(text);
-      return data;
+      return csv;
     })
     .catch(error => {
       console.error(error);
     });
-  console.log(data.value)
+  updateQuestion1()
 })
+
+const updateQuestion1 = () => {
+  console.log(data.value)
+  const filteredData = data.value.filter(d => Number(d.year) >= selectedTime0.value && Number(d.year) <= selectedTime1.value)
+  console.log(filteredData)
+  const groupedData = groupBy(filteredData, 'year')
+  console.log(groupedData)
+  const graphData = []
+  for (const [key, value] of Object.entries(groupedData)) {
+    graphData.push({
+      time: Number(key),
+      amount: value.reduce((acc, cur) => {
+        return acc + Number(cur['n_stopped'])
+      }, 0)
+    })
+  }
+  question1Total.value = graphData.reduce((acc, cur) => {
+    return acc + cur.amount
+  }, 0)
+  const width = 928;
+  const height = 500;
+  const marginTop = 30;
+  const marginRight = 0;
+  const marginBottom = 30;
+  const marginLeft = 40;
+  const svg = d3.select("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("viewBox", [0, 0, width, height])
+    .attr("style", "max-width: 100%; height: auto;");
+  // const svg = d3.select("svg").attr("width", width).attr("height", height);
+  // const g = svg.append("g");
+  // const x = d3
+  //   .scaleTime()
+  //   .domain(
+  //     d3.extent(graphData, d => parseTime(d.time))
+  //   )
+  //   .rangeRound([0, width]);
+  // const y = d3
+  //   .scaleLinear()
+  //   .domain(
+  //     d3.extent(graphData, d => d.amount)
+  //   )
+  //   .rangeRound([height, 0]);
+  const x = d3.scaleLinear()
+    .domain(d3.extent(graphData, (d) => d.time))
+    .range([marginLeft, width - marginRight])
+  const y = d3.scaleLinear()
+    .domain([0, d3.max(graphData, (d) => d.amount)])
+    .range([height - marginBottom, marginTop]);
+  svg.append("g")
+    .attr("class", "fill-primary-600")
+    .selectAll()
+    .data(graphData)
+    .join("rect")
+      .attr("x", (d) => x(d.time))
+      .attr("y", (d) => y(d.amount))
+      .attr("height", (d) => y(0) - y(d.amount))
+      .attr("width", (width / graphData.length) - 1);
+  svg.append("g")
+    .attr("transform", `translate(0,${height - marginBottom})`)
+    .call(d3.axisBottom(x).tickSizeOuter(0));
+  // Add the y-axis and label, and remove the domain line.
+  svg.append("g")
+    .attr("transform", `translate(${marginLeft},0)`)
+    .call(d3.axisLeft(y))
+    .call(g => g.select(".domain").remove())
+    .call(g => g.append("text")
+    .attr("x", -marginLeft)
+      .attr("y", 10)
+      .attr("fill", "currentColor")
+      .attr("text-anchor", "start")
+      .text("Stops"));
+}
 </script>
