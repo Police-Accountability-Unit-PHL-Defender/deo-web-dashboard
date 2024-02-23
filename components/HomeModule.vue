8c45e027f7748c91f9aac7cd6ccb0d3f868b3da6
Taichi Aritomo <taichiaritomo@gmail.com>
2024-02-23 17:38:51 -0500

    New driving equality image

--- components/HomeModule.vue
+++ components/HomeModule.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="my-20 grid md:grid-cols-2 gap-4 md:gap-10 items-center relative">
+  <div class="md:my-20 grid md:grid-cols-2 gap-4 md:gap-10 items-center relative">
     <div class="order-2 md:order-1" :class="{'md:order-2': props.reverse}">
       <div class="text-heading-2">
         <slot name="title"></slot>
       </div>
-      <div class="mt-4 md:mt-10">
-        <Button :href="props.ctaLink">Learn more</Button>
+      <div class="mt-4 md:mt-10 mb-6">
+        <Button :href="props.ctaLink" class="w-full md:w-auto">Learn more</Button>
       </div>
     </div>
-    <div class="relative flex items-center h-[300px] md:h-[430px] overflow-hidden order-1">
+    <div class="relative flex items-center md:h-[430px] order-1 -mx-4 md:mx-0">
       <slot name="image"></slot>
     </div>
   </div>
